0bc8fd79c483f79a7d7fdf71002817408271a85c
ghostq3 <135845471+Cprogram06@users.noreply.github.com>
2024-03-05 06:18:04 +0800

    data update

--- youtube/youtube_data.py
+++ youtube/youtube_data.py
@@ -27,7 +27,7 @@ def get_video_data(api_key, search_query):
     days_ago_str = days_ago.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     for i in range(6):
-        url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q=allintitle%3A{search_query}&type=video&maxResults=50&key={api_key}&publishedAfter={days_ago_str}"
+        url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={search_query}&type=video&maxResults=50&key={api_key}&publishedAfter={days_ago_str}"
         if i > 0:
             url += f"&pageToken={next_page_token}"
 
@@ -96,7 +96,6 @@ def get_video_data(api_key, search_query):
 
     return video_data
 
-
 def convert_to_csv(game, data):
     """Convert data to CSV."""
     df = pd.DataFrame(
@@ -116,9 +115,11 @@ def convert_to_csv(game, data):
     df.to_csv(f"{game_name}.csv")
 
 
-api_key = "AIzaSyBO5VBo-XQQD3R4fVY4CYJ8ExGtEy_jw9w"
+api_key = "AIzaSyBFzQD5yQeerxH05Hw9UU55La3HZCFUk04"
 
 game_list = [
+    'Pixels Online',
+    'Apeiron',
     'Townstar Game',
     'Parallel TCG',
     'Axie Infinity',
@@ -146,8 +147,6 @@ game_list = [
     'play_evio',
     'Spider Tanks',
     'League Kingdoms',
-    'Pixels Online',
-    'Apeiron'
 ]
 
 for game in game_list:
